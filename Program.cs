ad04850d5941114b7790f86654c796d8e24d3e17
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-15 13:55:25 -0500

    Initial commit of granola-sales project scaffold

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace granola_sales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
