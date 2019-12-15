a37cf7bfd717f6c41d6c26752d0848f08a5d85c7
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-15 14:12:48 -0500

    Implement algorithm

--- Program.cs
+++ Program.cs
@@ -5,9 +5,23 @@ namespace granola_sales
 {
     class Program
     {
+        // declare constants at class level
+        const double RETAIL_BAR = 1.5,
+                     BARS_PER_CASE = 100,
+                     STUDENT_GOV_PERCENTAGE = .1,
+                     WHOLESALE_CASE = 100;
         static void Main(string[] args)
         {
-            WriteLine("Hello World!");
+            // more compile-time initialization
+            double casesSold = 29,
+                   grossSales = casesSold * BARS_PER_CASE * RETAIL_BAR,
+                   studentGovDeduction = grossSales * STUDENT_GOV_PERCENTAGE,
+                   netProceeds = grossSales - studentGovDeduction;
+            WriteLine("COMPUTER CLUB GRANOLA SALES");
+            WriteLine("{0, 15} {1, 11}", "Cases sold:", casesSold);
+            WriteLine("{0, 15} {1, 11:C2}", "Gross sales:", grossSales);
+            WriteLine("{0, 15} {1, 11:C2}", "SGA deduction:", studentGovDeduction);
+            WriteLine("{0, 15} {1, 11:C2}", "Net proceeds:", netProceeds);
         }
     }
 }
