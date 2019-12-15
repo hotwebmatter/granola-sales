8bf229dfb3de33b7c912901a7134a6530399dc04
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-15 13:57:42 -0500

    Use static System.Console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace granola_sales
 {
@@ -6,7 +7,7 @@ namespace granola_sales
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Hello World!");
         }
     }
 }
